368c9d1c0de4d56aafdbd0f250fb655976af1181
Jessica Orozco <jessicaorozco@gmail.com>
2021-12-18 00:44:52 -0500

    apply format

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
   app.enableCors();
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
